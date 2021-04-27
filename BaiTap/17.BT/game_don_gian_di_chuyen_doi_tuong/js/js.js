dc77f056c4c65cbbc21ff8af03b44af59c1c1623
Thanh <thanhkybo@gmail.com>
2021-04-27 08:36:21 +0700

    add .

--- BaiTap/17.BT/game_don_gian_di_chuyen_doi_tuong/js/js.js
+++ BaiTap/17.BT/game_don_gian_di_chuyen_doi_tuong/js/js.js
@@ -13,20 +13,46 @@ function Hero(image, top, left, size){
     }
 
     this.moveRight = function(){
-        this.left += 60;
+        this.left += 3;
         console.log('ok: ' + this.left);
     }
-
+    this.moveDown = function () {
+        this.top += 3
+    }
+    this.moveLeft = function () {
+        this.left -= 3
+    }
+    this.moveUp = function () {
+        this.top -= 3
+    }
 }
 
 var hero = new Hero('zombie.png', 20, 30, 200);
-
+var dieukien = true
 function start(){
-    if(hero.left < window.innerWidth - hero.size){
-        hero.moveRight();
+    if (dieukien === true) {
+        if (hero.left < window.innerWidth - hero.size) {
+            hero.moveRight();
+        } else {
+            if (hero.top < window.innerHeight - hero.size) {
+                hero.moveDown();
+            } else {
+                dieukien = false
+            }
+        }
+    } else {
+        if (hero.left > 0) {
+            hero.moveLeft();
+        } else {
+            if (hero.top > 0) {
+                hero.moveUp();
+            } else {
+                dieukien = true
+            }
+        }
     }
     document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
+    setTimeout(start, 1)
 }
 
 start();
